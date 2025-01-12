12c8d0c8dbf333a08797e5e41580d92cf65b2530
Abhay Chaudhary <abhaycdry10@gmail.com>
2025-01-12 15:44:23 +0545

    commit

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 
-// Set the view engine to EJS
+// load the environment variable
+require('dotenv').config();
+
+// set the view engine to ejs
 app.set("view engine", "ejs");
 
-// Serve the public folder as static files
+// serve the public folder as static files
 app.use(express.static("public"));
 
-// Render the index template with default values for weather and error
+// render the index template with default values for weather and error
 app.get("/", (req, res) => {
   res.render("index", { weather: null, error: null });
 });
 
-// Handle the /weather route
+// handle the weather route
 app.get("/weather", async (req, res) => {
-  // Get the city from the query parameters
+  // get the city from the query parameters
   const city = req.query.city;
-  const apiKey = "835f59d2288a056992289cdf4aa8433d";
+  const apiKey = "835f59d2288a056992289cdf4aa8433d" ;
 
-  // Add your logic here to fetch weather data from the API
+  // add your logic here to fetch weather data from the API
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   let weather;
   let error = null;
@@ -30,12 +33,12 @@ app.get("/weather", async (req, res) => {
     weather = null;
     error = "Error, Please try again";
   }
-  // Render the index template with the weather data and error message
+  // render the index template with the weather data and error message
   res.render("index", { weather, error });
 });
 
-// Start the server and listen on port 3000 or the value of the PORT environment variable
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+// server connection
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
